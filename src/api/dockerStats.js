d1c6c366b2b3a9e04190132dec79a9a2aca05ccb
valuetodays <abcdefg20111111@gmail.com>
2025-09-14 20:39:05 +0800

    add

---
+++ src/api/dockerStats.js
@@ -0,0 +1,34 @@
+import request from '@/utils/request'
+
+export function chart(data) {
+  return request({
+    url: 'api/metricDockerStats/chart',
+    method: 'post',
+    data
+  })
+}
+export function add(data) {
+  return request({
+    url: 'api/metricDockerStats/add',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: 'api/metricDockerStats/delete',
+    method: 'post',
+    data: ids
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: 'api/metricDockerStats/edit',
+    method: 'post',
+    data
+  })
+}
+
+export default { add, edit, del }
