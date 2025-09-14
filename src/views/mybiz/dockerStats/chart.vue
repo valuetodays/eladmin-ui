866ead111942254bc2dbe1a63fb58fe70da10057
valuetodays <abcdefg20111111@gmail.com>
2025-09-14 21:06:34 +0800

    add

--- src/views/mybiz/dockerStats/chart.vue
+++ src/views/mybiz/dockerStats/chart.vue
@@ -4,6 +4,7 @@
 
 <script>
 import echarts from 'echarts'
+import dockerStatsApi from '@/api/dockerStats'
 
 export default {
   name: 'MemUsageChart',
@@ -13,42 +14,26 @@ export default {
     }
   },
   mounted () {
-    this.initChart()
+    this.fetchData()
   },
   methods: {
-    initChart () {
-      this.chart = echarts.init(document.getElementById('memChart'))
-      const option = {
-        title: {
-          text: 'Docker 容器内存使用 (MiB)'
-        },
-        tooltip: {
-          trigger: 'axis'
-        },
-        xAxis: {
-          type: 'category',
-          data: [
-            '2025-09-14 19:16:04',
-            '2025-09-14 19:24:04',
-            '2025-09-14 19:32:03',
-            '2025-09-14 19:40:04'
-          ]
-        },
-        yAxis: {
-          type: 'value',
-          name: 'MiB'
-        },
-        series: [
-          {
-            name: '内存使用',
-            type: 'line',
-            smooth: true,
-            data: [597.9, 597.9, 597.9, 597.9]
-          }
-        ]
-      }
-      this.chart.setOption(option)
-    }
-  }
+    fetchData () {
+      dockerStatsApi.chart({}).then(res => {
+        const times = res.data.map(d => d.statDatetime)
+        const values = res.data.map(d => d.memUsage2)
+        this.chart = echarts.init(document.getElementById('memChart'))
+        this.chart.setOption({
+          title: {
+            text: 'Docker 容器内存使用 (MB)'
+          },
+          tooltip: { trigger: 'axis'        },
+          xAxis: { type: 'category', data: times },
+          yAxis: { type: 'value', name: 'MB' },
+          series: [{ type: 'line', name: '内存使用', smooth: true, data: values }]
+        })
+      });
+    } // end of fetchData()
+    
+  } // end of methods:
 }
 </script>
