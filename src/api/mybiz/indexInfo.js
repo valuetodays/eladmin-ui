b1e1a045df2871ed5a1092e4ef1445746bfe63f8
valuetodays <abcdefg20111111@gmail.com>
2025-11-17 17:16:41 +0800

    add indexInfo

---
+++ src/api/mybiz/indexInfo.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+export function add(data) {
+  return request({
+    url: 'api/indexInfo/add',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: 'api/indexInfo/delete',
+    method: 'post',
+    data: ids
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: 'api/indexInfo/edit',
+    method: 'post',
+    data
+  })
+}
+
+export default { add, edit, del }
---
+++ src/views/mybiz/indexInfo/index.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <div v-if="crud.props.searchToggle">
+        <!-- 搜索 -->
+        <label class="el-form-item-label">编号</label>
+        <el-input v-model="query.code" clearable placeholder="编号" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">名称</label>
+        <el-input v-model="query.name" clearable placeholder="名称" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">区域（上海，深圳，北京，香港）</label>
+        <el-input v-model="query.region" clearable placeholder="区域" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <rrOperation :crud="crud" />
+      </div>
+      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <crudOperation :permission="permission" />
+      <!--表单组件-->
+      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+          <el-form-item label="编号" prop="code">
+            <el-input v-model="form.code" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="名称" prop="name">
+            <el-input v-model="form.name" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="区域" prop="region">
+            <el-input v-model="form.region" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="描述">
+            <el-input v-model="form.description" :rows="3" type="textarea" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="发布日期">
+            <el-date-picker v-model="form.releaseDate" type="datetime" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="数据基准日期">
+            <el-date-picker v-model="form.dataBaseDate" type="datetime" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="是否常见" prop="popularFlag">
+            <el-tooltip :content="'Switch value: ' + form.popularFlag" placement="top">
+              <el-switch
+                active-color="#13ce66"
+                active-text="启用"
+                :active-value="true"
+                inactive-color="#ff4949"
+                inactive-text="停用"
+                :inactive-value="false"
+                v-model="form.popularFlag">
+              </el-switch>
+            </el-tooltip>
+          </el-form-item>
+          <el-form-item label="建议的etf列表">
+            <el-input v-model="form.suggestEtfs" :rows="3" type="textarea" style="width: 370px;" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="text" @click="crud.cancelCU">取消</el-button>
+          <el-button :loading="crud.status.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        </div>
+      </el-dialog>
+      <!--表格渲染-->
+      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="id" label="主键" />
+        <el-table-column prop="code" label="编号" />
+        <el-table-column prop="name" label="名称" />
+        <el-table-column prop="region" label="区域" />
+        <el-table-column prop="description" label="描述" />
+        <el-table-column prop="releaseDate" label="发布日期" />
+        <el-table-column prop="dataBaseDate" label="数据基准日期" />
+        <el-table-column prop="popularFlag" label="是否常见">
+          <template slot-scope="scope">
+            {{ dict.label.switch_status_1_0[scope.row.popularFlag] }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="suggestEtfs" label="建议的etf列表" />
+        <el-table-column prop="createTime" label="创建时间" />
+        <el-table-column prop="updateTime" label="修改日期" />
+        <el-table-column prop="createUserId" label="创建者id" />
+        <el-table-column prop="updateUserId" label="更新者id" />
+        <el-table-column v-if="checkPer(['admin','indexInfo:edit','indexInfo:del'])" label="操作" width="150px" align="center">
+          <template slot-scope="scope">
+            <udOperation
+              :data="scope.row"
+              :permission="permission"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+import crudIndexInfo from '@/api/mybiz/indexInfo'
+import CRUD, { presenter, header, form, crud } from '@crud/crud'
+import rrOperation from '@crud/RR.operation'
+import crudOperation from '@crud/CRUD.operation'
+import udOperation from '@crud/UD.operation'
+import pagination from '@crud/Pagination'
+
+const defaultForm = { id: null, code: null, name: null, region: null, description: null, releaseDate: null, dataBaseDate: null, popularFlag: null, suggestEtfs: null, createTime: null, updateTime: null, createUserId: null, updateUserId: null }
+export default {
+  name: 'IndexInfo',
+  components: { pagination, crudOperation, rrOperation, udOperation },
+  mixins: [presenter(), header(), form(defaultForm), crud()],
+  dicts: ['switch_status_1_0'],
+  cruds() {
+    return CRUD({ title: 'dd', url: 'api/indexInfo/query', idField: 'id', sort: 'id,desc', crudMethod: { ...crudIndexInfo }})
+  },
+  data() {
+    return {
+      permission: {
+        add: ['admin', 'indexInfo:add'],
+        edit: ['admin', 'indexInfo:edit'],
+        del: ['admin', 'indexInfo:del']
+      },
+      rules: {
+        code: [
+          { required: true, message: '编号不能为空', trigger: 'blur' }
+        ],
+        name: [
+          { required: true, message: '名称不能为空', trigger: 'blur' }
+        ],
+        region: [
+          { required: true, message: '区域不能为空', trigger: 'blur' }
+        ],
+        popularFlag: [
+          { required: true, message: '是否常见不能为空', trigger: 'blur' }
+        ]
+      },
+      queryTypeOptions: [
+        { key: 'code', display_name: '编号' },
+        { key: 'name', display_name: '名称' },
+        { key: 'region', display_name: '区域' }
+      ]
+    }
+  },
+  methods: {
+    // 钩子：在获取表格数据之前执行，false 则代表不获取数据
+    [CRUD.HOOK.beforeRefresh]() {
+      return true
+    },
+    [CRUD.HOOK.beforeToAdd]() {
+      return true
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
