d12e936a8873df5448f42901d111485ba9af82bb
valuetodays <abcdefg20111111@gmail.com>
2026-02-12 00:18:08 +0800

    opt *.js

--- vue.config.js
+++ vue.config.js
@@ -63,6 +63,7 @@ module.exports = {
         test: /\.(js|css|html)?$/i, // 压缩文件格式
         filename: '[path].gz[query]', // 压缩后的文件名
         algorithm: 'gzip', // 使用gzip压缩
+        threshold: 10240, // 只有大于 10KB 的文件才压缩
         minRatio: 0.8 // 压缩率小于1才会压缩
       })
     ]
