2e2a2f43dbdfe1a1d57104bd990ba93d72eeee52
valuetodays <abcdefg20111111@gmail.com>
2026-02-11 21:00:54 +0800

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,11 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+  extends: [
+    'plugin:vue/recommended', 
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
 
   // add your custom rules here
   //it is base on https://github.com/vuejs/eslint-config-vue
