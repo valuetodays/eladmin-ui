5f35905876854f82c20cd6d7e040019c49ddd836
valuetodays <abcdefg20111111@gmail.com>
2026-02-25 09:25:47 +0800

    show k/d/j in table,

--- src/views/mybiz/stock-daily-indicator/index.vue
+++ src/views/mybiz/stock-daily-indicator/index.vue
@@ -36,9 +36,9 @@
         <el-table-column prop="code" label="code" />
         <el-table-column prop="statDate" label="统计日期" />
         <el-table-column prop="cci14" label="cci14" />
-        <el-table-column prop="k" label="k" />
-        <el-table-column prop="d" label="d" />
-        <el-table-column prop="j" label="j" />
+        <el-table-column prop="kdjK" label="k" />
+        <el-table-column prop="kdjD" label="d" />
+        <el-table-column prop="kdjj" label="j" />
         <el-table-column
           v-if="checkPer(['admin', 'stockDailyIndicator:edit', 'stockDailyIndicator:del'])"
           label="操作"
